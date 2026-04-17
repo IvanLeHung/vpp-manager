33974e01706ac50491caba1f6b72f31dd18449c2
IvanLeHung <lehunghanhchinh@gmail.com>
2026-04-17 11:50:53 +0700

    Add files via upload

--- src/pages/requests/RequestsList.tsx
+++ src/pages/requests/RequestsList.tsx
@@ -258,7 +258,7 @@ export default function RequestsList({ requests, currentUser, setViewMode, setAc
          </div>
       </div>
 
-      <div className="bg-white rounded-2xl shadow-[0_0_15px_rgba(0,0,0,0.03)] border border-slate-200 flex flex-col flex-1 overflow-hidden print:border-none print:shadow-none">
+      <div className="bg-white rounded-2xl shadow-[0_0_15px_rgba(0,0,0,0.03)] border border-slate-200 flex flex-col flex-1 overflow-hidden print:hidden">
         <div className="p-4 border-b border-slate-200 bg-slate-50/50 flex flex-col xl:flex-row gap-4 justify-between items-center print:hidden">
             <div className="flex flex-wrap gap-2 overflow-x-auto w-full xl:w-auto">
                <button onClick={() => setStatusFilter('ALL')} className={`px-4 py-2 rounded-lg font-bold text-sm whitespace-nowrap transition ${statusFilter==='ALL'?'bg-indigo-600 text-white shadow-md':'bg-white border border-slate-200 text-slate-600 hover:bg-slate-100'}`}>Tất cả</button>
